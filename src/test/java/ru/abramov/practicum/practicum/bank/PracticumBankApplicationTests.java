558d77b8eac19d6cad01dd3c3609a38e124e2744
Aleksandr Abramov <AleksAbramovWEB@yandex.ru>
2025-06-09 14:06:00 +0300

    Initial commit

---
+++ src/test/java/ru/abramov/practicum/practicum/bank/PracticumBankApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.abramov.practicum.practicum.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticumBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
