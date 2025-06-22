8f3294ffb4649ae7238c1e9b0b18b6006b4a85eb
Aleksandr Abramov <AleksAbramovWEB@yandex.ru>
2025-06-22 17:05:11 +0300

    - закончил с чартами

--- commands.sh
+++ commands.sh
@@ -7,20 +7,20 @@ helm uninstall postgres -n dev
 # Удаляет все PVC (PersistentVolumeClaim) в namespace dev.
 kubectl delete pvc -n dev --all
 # сборка
-./gradlew :services:cash:bootJar
-docker build --no-cache -t cash-practicum-bank:latest -f services/cash/Dockerfile .
-minikube image load cash-practicum-bank:latest
+./gradlew :services:front-ui:bootJar
+docker build --no-cache -t front-ui-practicum-bank:latest -f services/front-ui/Dockerfile .
+minikube image load front-ui-practicum-bank:latest
 kubectl rollout restart deployment cash -n dev
 # установить чарт
-helm install cash k8s/charts/cash \
+helm install front-ui k8s/charts/front-ui \
   -n dev --create-namespace \
-  -f k8s/charts/cash/values.yaml \
-  -f k8s/charts/cash/secret-values-dev.yaml
+  -f k8s/charts/front-ui/values.yaml \
+  -f k8s/charts/front-ui/secret-values-dev.yaml
 # Обновить чарт
-helm upgrade account k8s/charts/account \
+helm upgrade exchange-generator k8s/charts/exchange-generator \
   -n dev \
-  -f k8s/charts/account/values.yaml \
-  -f k8s/charts/account/secret-values-dev.yaml
+  -f k8s/charts/exchange-generator/values.yaml \
+  -f k8s/charts/exchange-generator/secret-values-dev.yaml
 # Подключиться к базе данных
 kubectl exec -it -n dev postgres-0 -- psql -U bank
 # Просмотр логов
@@ -33,6 +33,8 @@ kubectl get configmap account-config -n dev -o yaml
 # Удалить образ
 minikube ssh -- docker image rm blocker-practicum-bank:latest --force
 # Загрузить образ
-minikube image load account-practicum-bank:latest
+minikube image load blocker-practicum-bank:latest
 # Удалить все поды
 kubectl delete pods --all -n dev
+# Прокинуть порт наружу
+kubectl port-forward svc/keycloak 8080:8080 -n dev
