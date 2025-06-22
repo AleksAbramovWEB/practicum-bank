a60d171280198f16911573a03591844148d4339f
Aleksandr Abramov <AleksAbramovWEB@yandex.ru>
2025-06-23 00:37:15 +0300

    - настройка umbrella

--- commands.sh
+++ commands.sh
@@ -2,6 +2,8 @@
 kubectl rollout restart deployment account -n dev
 # посмотреть список подов в нейспесе дев
 kubectl get pods -n dev
+# посмотреть список по всему
+kubectl get all -n dev
 # удалить чат постгресс
 helm uninstall postgres -n dev
 # Удаляет все PVC (PersistentVolumeClaim) в namespace dev.
@@ -26,6 +28,7 @@ kubectl exec -it -n dev postgres-0 -- psql -U bank
 # Просмотр логов
 kubectl logs -n dev deploy/account
 kubectl logs -n dev deploy/account -f
+kubectl logs -n dev -l app=keycloak -f
 # Посмотреть секреты
 kubectl get secret postgres-secret -n dev -o yaml
 # Посмотреть конфиг
@@ -36,5 +39,31 @@ minikube ssh -- docker image rm blocker-practicum-bank:latest --force
 minikube image load blocker-practicum-bank:latest
 # Удалить все поды
 kubectl delete pods --all -n dev
+# Удалить все в нейспейсе
+kubectl delete all --all -n dev
+# Удалить нейспейс
+kubectl delete namespace dev
+# Удалить ресурс
+kubectl delete pv keycloak-theme-pv
 # Прокинуть порт наружу
 kubectl port-forward svc/keycloak 8080:8080 -n dev
+# Включить NGINX Ingress в Minikube
+kubectl minikube addons enable ingress
+# Применить namespace
+kubectl apply -f k8s/namespaces/dev.yaml
+# Подтянуть саб чарты в umbrella
+cd k8s/umbrella
+helm dependency update
+# или
+helm dependency build
+# установить umbrella
+ helm upgrade --install practicum-bank . \
+                                      -n dev --create-namespace \
+                                      -f values-dev.yaml \
+                                      -f secret-values-dev.yaml
+ # проброска тунеля 19901508Oi!
+ minikube tunnel
+ # остановить
+ minikube stop
+ # запустить
+ minikube start
